d3f62aca24afa8d6be4fae88b1f66032545dc38d
Chiefjack <jackygems2003@gmail.com>
2019-04-05 21:48:54 +0100

    Vastly Improved Menu. Completed Class Lists.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,227 @@ $('#staff_header_id').on('click', function() {
   $('html, body').animate({scrollTop: images},900);
 });
 
+$('#tutors_header_id').on('click', function() {
+  const images = $('#tutors_header').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_air_deco_id').on('click', function() {
+  const images = $('#tutors_air_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_block_deco_id').on('click', function() {
+  const images = $('#tutors_block_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_tech_id').on('click', function() {
+  const images = $('#tutors_tech').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_glow_id').on('click', function() {
+  const images = $('#tutors_glow').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_core_id').on('click', function() {
+  const images = $('#tutors_core').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_modern_id').on('click', function() {
+  const images = $('#tutors_modern').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_design_id').on('click', function() {
+  const images = $('#tutors_design').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_art_id').on('click', function() {
+  const images = $('#tutors_art').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_effect_id').on('click', function() {
+  const images = $('#tutors_effect').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_gameplay_id').on('click', function() {
+  const images = $('#tutors_gameplay').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_trigger_id').on('click', function() {
+  const images = $('#tutors_trigger').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_3dl_id').on('click', function() {
+  const images = $('#tutors_3dl').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#tutors_colour_id').on('click', function() {
+  const images = $('#tutors_colour').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_header_id').on('click', function() {
+  const images = $('#teachers_header').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_air_deco_id').on('click', function() {
+  const images = $('#teachers_air_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_block_deco_id').on('click', function() {
+  const images = $('#teachers_block_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_tech_id').on('click', function() {
+  const images = $('#teachers_tech').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_glow_id').on('click', function() {
+  const images = $('#teachers_glow').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_core_id').on('click', function() {
+  const images = $('#teachers_core').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_modern_id').on('click', function() {
+  const images = $('#teachers_modern').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_design_id').on('click', function() {
+  const images = $('#teachers_design').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_art_id').on('click', function() {
+  const images = $('#teachers_art').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_effect_id').on('click', function() {
+  const images = $('#teachers_effect').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_gameplay_id').on('click', function() {
+  const images = $('#teachers_gameplay').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_trigger_id').on('click', function() {
+  const images = $('#teachers_trigger').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_3dl_id').on('click', function() {
+  const images = $('#teachers_3dl').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#teachers_colour_id').on('click', function() {
+  const images = $('#teachers_colour').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_header_id').on('click', function() {
+  const images = $('#classes_header').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_air_deco_id').on('click', function() {
+  const images = $('#classes_air_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_block_deco_id').on('click', function() {
+  const images = $('#classes_block_deco').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_tech_id').on('click', function() {
+  const images = $('#classes_tech').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_glow_id').on('click', function() {
+  const images = $('#classes_glow').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_core_id').on('click', function() {
+  const images = $('#classes_core').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_modern_id').on('click', function() {
+  const images = $('#classes_modern').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_design_id').on('click', function() {
+  const images = $('#classes_design').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_art_id').on('click', function() {
+  const images = $('#classes_art').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_effect_id').on('click', function() {
+  const images = $('#classes_effect').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_gameplay_id').on('click', function() {
+  const images = $('#classes_gameplay').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_trigger_id').on('click', function() {
+  const images = $('#classes_trigger').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_3dl_id').on('click', function() {
+  const images = $('#classes_3dl').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+$('#classes_colour_id').on('click', function() {
+  const images = $('#classes_colour').position().top;
+  $('html, body').animate({scrollTop: images},900);
+});
+
+
+// Navbar
+
+function navbar_responsive() {
+  var x = document.getElementById("id_navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
 
 /*Scroll to top when arrow up clicked*/
 $(window).scroll(function() {
@@ -32,6 +253,10 @@ $(document).ready(function() {
     });
 
 });
+
+function go_back() {
+  window.history.back();
+}
 
 
 /*Scroll to bottom when arrow down clicked*/
